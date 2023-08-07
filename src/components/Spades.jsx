fc934e013b662357a7048859b3c332588d63fc7c
Doug <124453486+takilesi@users.noreply.github.com>
2023-08-07 00:35:24 -0400

    spades function added

--- src/components/Spades.jsx
+++ src/components/Spades.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState} from 'react';
 import { AppContext } from "../context/folio_context";
+// import HeartsL from '../components/HeartsL'
+import SpadesC from '../components/SpadesC'
+// import HeartsR from '../components/HeartsR'
 
 function Spades() {
+
+    const [box, setBoxH] = useState(0);
+
     return (
-        <div className='bottomBox'> 
-            Spades
-            <div className="scrollContainer">
-
-                <div className="bottomBoxBulletPoints">
-                    left
-                </div>
-                <div className="bottomBoxBulletPoints">
-                    center 
-                </div>
-                <div className="bottomBoxBulletPoints">
-                    right 
-                </div>
-
+        <div className="heartsJsx"> 
+            <button className="heartsButtons" onClick={()=>setBoxH(1)} >Landmark Education</button>
+            {/* <button className="heartsButtons" onClick={()=>setBoxH(2)} >Vocal Confluence</button>
+            <button className="heartsButtons" onClick={()=>setBoxH(3)} >Rachel Carson Trail Challenge</button>   */}
+            <div className="bottomBox">
+                {box === 1 && <SpadesC /> }
+                {/* {box === 2 && <HeartsC /> }
+                {box === 3 && <HeartsR /> } */}
             </div>
         </div>
       )
