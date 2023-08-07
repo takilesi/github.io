dbfb6e8c7c198e63c8fc66403addf0c2f8c76ca4
Doug <124453486+takilesi@users.noreply.github.com>
2023-08-07 01:55:09 -0400

    added hiking locations

--- src/components/ClubsC.jsx
+++ src/components/ClubsC.jsx
@@ -4,8 +4,19 @@ import meetup from '../meetup.jpg';
 function ClubsC() {
   return (
     <div className="">
-    <img className="diamond" style={{width: '100%'}} src={meetup} alt=""/>
-</div>
+        <p className="hikes">Locations: </p>
+        <p className="hikes">Hays Woods</p>
+        <p className="hikes">Riverview Park</p>
+        <p className="hikes">Crafton Park</p>
+
+        <p className="hikes">Thornburg Conservatoin Park</p>
+        <p className="hikes">Downtown</p>
+        <p className="hikes">Beachwood Farms</p>
+        <p className="hikes">Hartwood Acres</p>
+          
+        <img className="diamond" style={{width: '100%'}} src={meetup} alt=""/>
+ 
+    </div>
   )
 }
 
