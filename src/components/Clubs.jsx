2cb7f7024434598723fb23dd6a16f75f9287067f
Doug <124453486+takilesi@users.noreply.github.com>
2023-08-07 01:30:19 -0400

    adding clubs

--- src/components/Clubs.jsx
+++ src/components/Clubs.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import ClubsC from '../components/ClubsC'
 
 function Clubs() {
+
+    const [box, setBoxH] = useState(0);
+
     return (
-        <div> 
-            <h1>Clubs</h1>
-         
+        <div className="heartsJsx"> 
+            <button className="heartsButtons" onClick={()=>setBoxH(1)} >Landmark Education</button>
+            {/* <button className="heartsButtons" onClick={()=>setBoxH(2)} >Vocal Confluence</button>
+            <button className="heartsButtons" onClick={()=>setBoxH(3)} >Rachel Carson Trail Challenge</button>   */}
+            <div className="bottomBox">
+                {box === 1 && <ClubsC /> }
+                {/* {box === 2 && <HeartsC /> }
+                {box === 3 && <HeartsR /> } */}
+            </div>
         </div>
       )
     }
